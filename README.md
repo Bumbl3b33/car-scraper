b7f109d6e7cd544e00d3df4f20edc97ffa197ff1
Azam Fahmy <azamf@infyinnos.com>
2022-10-03 01:16:40 +0530

    Write to files after scraping

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ This tool can be used to log data about cars for future reference and track pric
 - Create a virtual environment `python -m venv <name-of-venv>`
 - Download the webdriver. For Edge -> <https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/>
 - Place the webdriver file in the same folder as the `.py` file
+- TODO:  Requirements File
--- scraper.py
+++ scraper.py
@@ -8,26 +8,30 @@ try:
     LINK_TO_SCRAPE = os.getenv('LINK_TO_SCRAPE')
     SCRAPE_COUNT = int(os.getenv('SCRAPE_COUNT'))
     OUTPUT_FILE = os.getenv('OUTPUT_FILE')
+    CAR_DETAIL_COUNT = int(os.getenv('CAR_DETAIL_COUNT'))
 except:
     raise ValueError("Failed to load env variables")
 
 driver = webdriver.Edge()
 driver.get(LINK_TO_SCRAPE + '/axio')
 
+cars = ""
 for i in range(1,SCRAPE_COUNT):
         
     car = []
     name = driver.find_element(By.XPATH, '//*[@id="content"]/ul/li[' + str(i) +  ']/h2').text
     car.append(name)
-    for j in range(1,5):
+    for j in range(1,CAR_DETAIL_COUNT):
         detail = driver.find_element(By.XPATH, '//*[@id="content"]/ul/li[' + str(i) +  ']/div[2]/div['+ str(j) + ']').text 
         car.append(detail)
+    
+    car_as_csv = repr(car).removeprefix("[").removesuffix("]") + "," + "\n"
+    cars = cars + car_as_csv
 
-    try:
-        f = open("cars.txt", mode='a', encoding='utf-8')
-        car_as_csv = repr(car).removeprefix("[").removesuffix("]") + "," + "\n"
-        f.write(car_as_csv)
-    finally:
-        f.close()
+try:
+    f = open(OUTPUT_FILE, mode='a', encoding='utf-8')
+    f.write(cars)
+finally:
+    f.close()
 
 driver.quit()
